2c2d710bb0d14183ca60d03e00579fd5c00fd5da
marcinbrach02 <46870206+marcinbrach02@users.noreply.github.com>
2022-03-13 16:07:12 +0100

    Added the first lesson and answers for excercises.

---
+++ PYTHON/hc_course/zajecia1.py
@@ -0,0 +1,31 @@
+print("Marcin")
+print(5)
+print(1+3)
+print(3*15*99/121+3)
+
+print(5//3) # -  tylko całości w wyniku
+
+print("Mam" + "kota")
+print("ha"*3)
+# print("ha"*2.5) - błąd bo mnożenie tylko dla liczb całkowitych
+
+print(2.5)
+
+# konweresja do odpowiedniego typu:
+print("teraz mamy rok " + str(2022.5))
+print(int("2022") + 1)
+print(float("2022.5") + 1)
+
+print("Mam\n" + "kota" + "\ni psa")
+print("Mam\t" + "kota" + "\ti" + "\tpsa")
+
+print("c:\\nowy folder")
+
+print("mama")
+print('mama')
+
+#pojedynczy cudzysłów wewnątrz napisu:
+print('Moja ulubiona ksiązka to: \"Ojciec chrzestny"')
+
+print("hahaha", end="")
+print("hihihi")
